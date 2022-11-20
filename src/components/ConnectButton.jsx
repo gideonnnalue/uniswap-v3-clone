ad625679fac36011f8c48071faec7af1b80e85cd
Gideon Nnalue <emekannalue@gmail.com>
2022-11-20 21:18:24 +0100

    setup modal for config

---
+++ src/components/ConnectButton.jsx
@@ -0,0 +1,24 @@
+import PageButton from "./PageButton";
+
+const ConnectButton = (props) => {
+  const { isConnected, signerAddress, getSigner, provider } = props;
+  const displayAddress = `${signerAddress?.substring(0,10)}...`;
+  return (
+    <>
+      {isConnected() ? (
+        <div className="buttonContainer">
+          <PageButton name={displayAddress} />
+        </div>
+      ) : (
+        <div
+          className="btn my-2 connectButton"
+          onClick={() => getSigner(provider)}
+        >
+          Connect Wallet
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ConnectButton;
